092e10d3b0a326d0c89d1d1f4a40d3267c11bad3
Hermann <hermann.kaebi@gmail.com>
2021-04-02 10:12:15 +0300

    Another test
    Testing another path strategy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,10 +12,10 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7bf84e807e49244155904e5a58657146",
-"/": "7bf84e807e49244155904e5a58657146",
+"index.html": "fe382ed3ad4f8f895b6bfc6781bf8243",
+"/": "fe382ed3ad4f8f895b6bfc6781bf8243",
 "main.dart.js": "9a77e6ab05db9a02b799cda95bcf765f",
-"manifest.json": "c6fd077880f19afaa6ecd0e6db57f211",
+"manifest.json": "42b923af76f9a6e525bf0c7ce9411312",
 "version.json": "f09a185c125b0ddd549a1c3263c58e77"
 };
 
